38f5b4099485f75e63a431330861d82e061d1c87
=Alberto <=jlr0013@alu.medac.es>
2024-11-08 14:18:17 +0100

    Aplicación de test de preguntas con varias Activities

---
+++ app/src/main/java/com/example/ejercicioandroid9/Almacen.java
@@ -0,0 +1,22 @@
+package com.example.ejercicioandroid9;
+
+public class Almacen {
+    private String nombre;
+    private int contador=0;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+}
